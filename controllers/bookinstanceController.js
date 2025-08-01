601e77c8fb5b50b6d8c92ce6862a1165ab4c5405
GrimProg <vavilovila22@icloud.com>
2025-08-01 23:33:17 +0600

    Remove .env from repo

---
+++ controllers/bookinstanceController.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const { body, validationResult } = require("express-validator");
+const Book = require("../models/book");
+
+
+const BookInstance = require("../models/bookinstance");
+
+/** @typedef {import("express").Request} Request */
+/** @typedef {import("express").Response} Response */
+/** @typedef {import("express").NextFunction} NextFunction */
+
+/**
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+
+exports.bookinstance_list = async (req, res, next) => {
+  const allBookInstances = await BookInstance.find().populate("book").exec();
+
+  res.render("bookinstance_list", {
+    title: "Book Instance List",
+    bookinstance_list: allBookInstances,
+  });
+};
+
+
+exports.bookinstance_detail = async (req, res, next) => {
+  const bookInstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
+    const err = new Error("Book copy not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("bookinstance_detail", {
+    title: "Book:",
+    bookinstance: bookInstance,
+  });
+};
+
+exports.bookinstance_create_get = async (req, res, next) => {
+  const allBooks = await Book.find({}, "title").sort({ title: 1 }).exec();
+
+  res.render("bookinstance_form", {
+    title: "Create BookInstance",
+    book_list: allBooks,
+  });
+};
+
+
+// Handle BookInstance create on POST.
+exports.bookinstance_create_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render form again with sanitized values and error messages.
+      const allBooks = await Book.find({}, "title").sort({ title: 1 }).exec();
+
+      res.render("bookinstance_form", {
+        title: "Create BookInstance",
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      });
+      return;
+    }
+
+    // Data from form is valid
+    await bookInstance.save();
+    res.redirect(bookInstance.url);
+  },
+];
+
+
+exports.bookinstance_delete_get = async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+};
+
+exports.bookinstance_delete_post = async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+};
+
+exports.bookinstance_update_get = async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: BookInstance update GET");
+};
+
+exports.bookinstance_update_post = async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: BookInstance update POST");
+};
---
+++ controllers/genreController.js
@@ -0,0 +1,85 @@
+const { body, validationResult } = require("express-validator");
+const Genre = require("../models/genre");
+const Book = require("../models/book");
+
+exports.genre_list = async (req, res, next) => {
+  const allGenres = await Genre.find().sort({ name: 1 }).exec()
+  res.render("genre_list", {
+    title: "Genre List",
+    genre_list: allGenres,
+  });
+};
+
+exports.genre_detail = async (req, res, next) => {
+  const [genre, booksInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+  if (genre === null) {
+    const err = new Error("Genre not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("genre_detail", {
+    title: "Genre Detail",
+    genre,
+    genre_books: booksInGenre,
+  });
+};
+
+
+exports.genre_create_get = (req, res, next) => {
+  res.render("genre_form", { title: "Create Genre" });
+};
+
+
+exports.genre_create_post = [
+  body("name", "Genre name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const genre = new Genre({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      res.render("genre_form", {
+        title: "Create Genre",
+        genre,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    const genreExists = await Genre.findOne({ name: req.body.name })
+      .collation({ locale: "en", strength: 2 })
+      .exec();
+    if (genreExists) {
+      res.redirect(genreExists.url);
+      return;
+    }
+
+    await genre.save();
+    res.redirect(genre.url);
+  },
+];
+
+
+exports.genre_delete_get = async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Genre delete GET");
+};
+
+exports.genre_delete_post = async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Genre delete POST");
+};
+
+exports.genre_update_get = async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Genre update GET");
+};
+
+exports.genre_update_post = async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Genre update POST");
+};
